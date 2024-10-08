8e9891fa1a809adbca74bd392c6c47b085d934ae
Hector Gomez <gomezgodoyhector@gmail.com>
2024-10-07 21:19:33 -0300

    Se añaden elementos para manejar el modo claro/oscuro

--- app/src/main/java/com/example/desafio02/FullscreenActivity.kt
+++ app/src/main/java/com/example/desafio02/FullscreenActivity.kt
@@ -1,11 +1,15 @@
 package com.example.desafio02
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.example.desafio02.databinding.ActivityFullscreenBinding
 
 class FullscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFullscreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,12 +18,17 @@ class FullscreenActivity : AppCompatActivity() {
         binding = ActivityFullscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Inicializamos el ConstraintLayout desde View Binding
-        fullscreenContent = binding.fullscreenContent
+        // Detectamos el modo actual (claro u oscuro)
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
 
+        // Cambiar colores dinámicamente el fondo según el modo claro u oscuro
+        val fullscreenContent: ConstraintLayout = binding.fullscreenContent
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            fullscreenContent.setBackgroundColor(ContextCompat.getColor(this, R.color.colorBackgroundDark))
+            binding.vectorImageView.setColorFilter(ContextCompat.getColor(this, R.color.vector_color_dark), android.graphics.PorterDuff.Mode.SRC_IN)
+        } else {
+            fullscreenContent.setBackgroundColor(ContextCompat.getColor(this, R.color.colorBackground))
+            binding.vectorImageView.setColorFilter(ContextCompat.getColor(this, R.color.vector_color), android.graphics.PorterDuff.Mode.SRC_IN)
         }
-
-    // Cambiamos el tipo de la variable fullscreenContent a ConstraintLayout
-    private lateinit var binding: ActivityFullscreenBinding
-    private lateinit var fullscreenContent: ConstraintLayout
+    }
 }
